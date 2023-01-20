e0999f9b7e2573be5eedd548272354cd50b3ac58
fnanoia <francisconanoia@hotmail.com>
2023-01-20 17:45:15 -0300

    deploy ready

--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -8,7 +8,7 @@ const Contact = () => {
                 </div>
                 <input className='bg-[#ccd6f6] p-2' type="text" placeholder='Name' name='name' />
                 <input className='my-4 p-2 bg-[#ccd6f6]' type="email" placeholder='Email' name='email' />
-                <textarea className='bg-[#ccd6f6] p-2' name="message" rows="10" placeholder='Message'></textarea>
+                <textarea className='bg-[#ccd6f6] p-2' name="message" rows={10} placeholder='Message'></textarea>
                 <button className='text-white border-2 hover:bg-[#A81D33] hover:border-[#A81D33] px-4 py-3 my-8 mx-auto flex items-center'>Submit</button>
             </form>
         </div>
